8bfd3c63526a668828109414e0a6fc1c8449fceb
mukanjy0 <mukanjyo29@protonmail.com>
2024-02-06 08:02:38 -0500

    added validation test for Rainy Research

--- rainyResearch.cpp
+++ rainyResearch.cpp
@@ -5,6 +5,8 @@
 #include <set>
 #include <climits>
 #include <unordered_map>
+#include <random>
+#include <cassert>
 using namespace std;
 
 class rainy_research {
@@ -29,4 +31,35 @@ public:
         else return 0;
     }
 };
+
+void test() {
+    random_device rd;
+    mt19937 rng(rd());
+    uniform_real_distribution distLat(-180., 180.);
+    uniform_int_distribution distTime(1, 1000);
+    uniform_int_distribution distR(1, 100000);
+
+    rainy_research res;
+    unordered_map<double,set<pair<int,int>>> um;
+    for (int i = 0; i < 1000; ++i) {
+        double lat = distLat(rng);
+        int t = distTime(rng), r = distR(rng);
+        res.record_data(r, lat, t);
+        um[lat].emplace(t, r);
+    }
+    for (int i = 0; i < 1000; ++i) {
+        double lat = distLat(rng);
+        int t = distTime(rng);
+        auto ans = res.peak_rainfall(lat, t);
+        int correct_ans = 0;
+        for (auto it = um[lat].lower_bound({t, INT_MIN}); it != um[lat].end(); ++it)
+            correct_ans = max(correct_ans, it->second);
+        assert(ans == correct_ans);
+    }
+}
+
+int main() {
+    test();
+    return 0;
+}
 
